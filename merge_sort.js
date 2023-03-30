3239718dc24f7d7cb6846afde2490565d357b0be
Godswillben <amogugodswill99@gmail.com>
2023-03-30 20:00:36 +0100

    merge sort

--- merge_sort.js
+++ merge_sort.js
@@ -16,7 +16,20 @@ function mergeSort(array){
 }
 
 function merge(left, right){
-    //
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while(leftIndex < left.length && rightIndex < right.length){
+        if (left[leftIndex] < right[rightIndex]){
+            result.push(left[leftIndex]);
+            leftIndex++
+        }else{
+            result.push(right[rightIndex])
+            rightIndex++
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
 
 const answer = mergeSort(numbers);
