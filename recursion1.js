55fabe6ddf2e657da5da15eeb57dc2f8dd13f145
Godswillben <amogugodswill99@gmail.com>
2023-03-31 19:36:27 +0100

    recursion1.js

--- recursion1.js
+++ recursion1.js
@@ -7,3 +7,14 @@ function findFactorialRecursive(number){
 }
 
 findFactorialRecursive(5)
+
+// fibonacci
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+function fibonacciRecusive(n){
+    if(n < 2){
+        return n
+    }
+    return fibonacciRecusive(n-1)  + fibonacciRecusive(n-2)
+}
+
+// sorting 
