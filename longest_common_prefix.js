2397a5b036775a1ef438ac3ec21c9d696e125077
Godswillben <amogugodswill99@gmail.com>
2023-04-18 10:50:01 +0100

    longest common prefix complete solution

--- longest_common_prefix.js
+++ longest_common_prefix.js
@@ -1,4 +1,28 @@
 // find longest prefix
-var longestCommonPrefix = function(strs) {
-    
+
+// brute force
+var longestCommonPrefix = function (strs) {
+    // check if argument is not list: return ""
+    if (!Array.isArray(strs) ) {
+        return ""
+    }
+    output = ""
+    // we loop through the first object 
+    for (i = 0; i < strs[0].length; i++) {
+        // we loop through all object
+        for (j = 1; j < strs.length; j++) {
+            // we check if current index of first object
+            if (strs[0][i] !== strs[j][i]) {
+                console.log(output)
+                return output
+            }
+        }
+        // if it get's here add currentIndex to output
+        output = output + strs[0][i];
+    }
+    console.log(output)
+    return output
 };
+
+longestCommonPrefix(["flower","flow","flight"])
+longestCommonPrefix(["dog","racecar","car"])
