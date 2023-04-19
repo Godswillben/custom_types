5e91474ddb7ffb2b22bb6c142ddc2885571a5833
Godswillben <amogugodswill99@gmail.com>
2023-04-19 09:58:46 +0100

    append and apprintList paramenters

--- linked_list/data_type.js
+++ linked_list/data_type.js
@@ -17,9 +17,30 @@ class LinkedList{
         this.length++;
         return this
     }
+    prepend(value){
+        const newNode = {
+            value: value,
+            next: null
+        };
+        newNode.next = this.head;
+        this.head = newNode
+        this.length++;
+        return this
+    }
+    printList(){
+        const array = [];
+        let currentNode =  this.head;
+        while (currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    insert(index, value){}
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.prepend(1)
 console.log(myLinkedList)
