024024ca713f252f6f42c0681b57fa63e4a7c436
Godswillben <amogugodswill99@gmail.com>
2023-04-24 19:01:36 +0100

    changes

--- linked_list/data_type2.js
+++ linked_list/data_type2.js
@@ -32,7 +32,74 @@ class LinkedList {
         this.length++;
         return this;
     }
-}    
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    // insert(index, value){
+    //     if (index > this.length){
+    //         this.append(value)
+    //     }
+    //     if(index === 0){
+    //         this.prepend(value)
+    //     }
+    //     const newNode = {
+    //         value: value,
+    //         next: null
+    //     }
+    //     let currentNode = this.head;
+    //     let counter = 0;
+    //     while(true){
+    //         if (index === counter){
+
+    //         }
+    //     }
+        
+    // } // my insert method though not completed
+
+    insert(index, value){
+        // check params
+        if (index >= this.length){
+            return this.append(value);
+        }
+        if(index === 0){
+            this.prepend(value)
+        }
+        const newNode = {
+            value: value,
+            next: null
+        };
+        const leader = this.traverseToIndex(index - 1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this.printList()
+    }
+    traverseToIndex(index){
+        // check params
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index){
+            currentNode = currentNode.next;
+            counter++
+        }
+        return currentNode;
+    }
+    remove(index){
+        // check params
+        const leader = this.traverseToIndex(index-1)
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+        this.length--;
+        return this.printList();
+    }
+} 
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
