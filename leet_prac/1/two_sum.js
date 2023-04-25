f93cfb6c5695111160903a5f080146a1ca7b5e5e
Godswillben <amogugodswill99@gmail.com>
2023-04-25 13:27:06 +0100

    prac1: two sums solution

---
+++ leet_prac/1/two_sum.js
@@ -0,0 +1,41 @@
+/* 
+    Question
+    give a list of integers returns indecies of values that
+    add up to target
+*/
+
+// figure out the edge cases
+/*
+ no two value can return target
+ an empty list or list with only one value returns null
+*/
+
+/* 
+    Test Case
+    [1, 3, 7, 9, 2];   t=11;    result=[3,4]
+    [1, 3, 7, 9, 2];   t=25;    result=null
+    [];   t=1;    result=null
+    [5];   t=5;    result=null
+    [1,6];   t=7;    result=[0,1]
+*/
+
+/** 
+ * Come up with a solution logically without
+ * thinking of the code
+ * */ 
+
+const findTwoSum = function(nums, target){
+    for(let p1=0; p1 < nums.length; p1++){
+        const numberToFind = target - p1;
+        for(p2=p1+1; p2<nums.length; p2++){
+            if(numberToFind === nums[p2]){
+                return [p1, p2]
+            }
+        }
+    }
+    return null
+}
+
+/**
+ * check for typo or mistke in code
+ */
