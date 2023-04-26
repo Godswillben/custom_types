3e8566a4dac1363ddf146105b9f307a74da9752e
Godswillben <amogugodswill99@gmail.com>
2023-04-26 21:20:01 +0100

    reverse linked list description

---
+++ leet_prac/1/reverse_linkedList.js
@@ -0,0 +1,12 @@
+/**
+ * Givien a linked List return the reverse
+ */
+
+/**
+ * What do we return if we are given null
+ * -> we return null
+ * What do we return if we are given a single node
+ * -> we return a single node
+ */
+
+
