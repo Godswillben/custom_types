eefe4c750b6fc3dfde0d9934e55d94cf48e30c80
Godswillben <amogugodswill99@gmail.com>
2023-04-27 13:06:46 +0100

    queue with stacks enqueue and dequeue

---
+++ leet_prac/1/queues_with_stacks.js
@@ -0,0 +1,21 @@
+class QueuesWithStacks{
+    constructor(){
+        this.in=[];
+        this.out=[];
+    }
+
+    enqueue(val){
+        this.in.push(val)
+    }
+ 
+    dequeue(){
+        if(this.out.length === 0){
+            while(this.in.length){
+                this.out.push(this.in.pop())
+            }
+        }
+        return this.out.pop()
+    }
+}
+// [1,2,3,4,5]
+// [5, 4, 3, 2, 1]
