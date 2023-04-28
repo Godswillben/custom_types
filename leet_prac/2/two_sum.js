5c18e68fcd7f14580c2ecfd129803e7a5350834c
Godswillben <amogugodswill99@gmail.com>
2023-04-28 11:34:51 +0100

    two sum brute for solution

--- leet_prac/2/two_sum.js
+++ leet_prac/2/two_sum.js
@@ -25,3 +25,21 @@
  * [5]  t=5     null
  * [1,6]  t=7     [0,1]
  */
+
+/** Try To Come up with solution without thinking about */
+
+/** Brute Force Solution */
+
+const  findTwoSum = function (nums, target){
+    for (let p1=0; p1 < nums.length; p1++){
+        const numberToFind = target - nums[p1]
+        for (p2=p1 + 1; p2<nums.length; p2++){
+            if(numberToFind === nums[p2]){
+                return [p1, p2]
+            }
+        }
+    }
+    return null;
+}
+
+/** Double check for errors */
