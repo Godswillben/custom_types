86c598424f5dbb2e8e14c7f8ad7036d37cb920be
Godswillben <amogugodswill99@gmail.com>
2023-04-28 09:55:57 +0100

    question test case of two sums

---
+++ leet_prac/2/two_sum.js
@@ -0,0 +1,21 @@
+/** Question
+ * Provide a list of integers return indices that
+ * sums up to target
+ */
+
+/** Verify Constraints
+ * Are all numbers going to be positive
+ * -> Yes all numbers are positive no negative numbers
+ * Are there going to be duplicates do we account for duplicates 
+ * that might add up to the targets, depending on the target
+ * -> No, there are no duplicates
+ * Will there always be soultion... would two values always add up to the
+ * Target
+ * -> No, there may not always be a solution
+ * What do we return if there is no solution
+ * -> We return Null
+ * Can there be multiple pair that sum up to the target?
+ * -> No, only 1 pair of numbers will add up to the target
+ */
+
+
