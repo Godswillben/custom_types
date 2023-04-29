c461c1218320bd7653ef436031032f52ac57f879
Godswillben <amogugodswill99@gmail.com>
2023-04-29 17:43:40 +0100

    Code solution

---
+++ leet_prac/2/valid_parenthesis.js
@@ -0,0 +1,19 @@
+/** Question
+ * Given a string containing only parenthesis, determine if it is valid.
+ * The string is valid if all parentheses close.
+ */
+
+/** Verify Constraints
+ * Does an empty count as valid
+ * -> Yes an empty string count as valid
+ */
+
+/** Test Cases
+ * "" -> True
+ * "{([])}" -> True
+ * "{([]"   ->  False
+ * "{[(])}" -> False
+ * "{[]()}" -> True
+ */
+
+/** Come up with a solution without thinking about the code */
