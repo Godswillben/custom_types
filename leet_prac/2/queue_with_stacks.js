bf7c82ec639f954de7cfa520f1fbf8e952edd9cd
Godswillben <amogugodswill99@gmail.com>
2023-04-29 20:00:52 +0100

    queue with stacks solution

---
+++ leet_prac/2/queue_with_stacks.js
@@ -0,0 +1,44 @@
+/** Question 
+ * Implement class Queue with stacks, 
+ * queue methods you need to implement are enqueue, dequeue, peek, and empty
+ */
+
+/** Very Constraints
+ * Do the queue methods we implement need to perform at the same 
+ * space and time complexity of an original queue?
+ * -> No, but they should be as performant as possible
+ */
+
+class QueueWithStacks {
+    constructor(){
+        this.in=[];
+        this.out=[];
+    }
+
+    enqueue(val){
+        this.in.push(val)
+    }
+
+    dequeue(){
+        if(this.out.length === 0){
+            while(this.in.length){
+                this.out.push(this.in.pop())
+            }
+        }
+        return this.out.pop()
+    }
+
+    peek(){
+        // if(this.out.length === 0){
+        //     return this.in[0]
+        // }
+        // return this.out[this.out.length - 1]
+        if(this.out.length === 0){
+            while(this.in.length){
+                this.out.push(this.in.pop())
+            }
+        }
+        return this.out[this.out.length - 1]
+    }
+    
+}
