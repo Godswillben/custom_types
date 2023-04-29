c1e63c9bdbcda157e6ee85816f7e5b92f903d143
Godswillben <amogugodswill99@gmail.com>
2023-04-29 18:32:18 +0100

    Valid parenthesis code solution

--- leet_prac/2/valid_parenthesis.js
+++ leet_prac/2/valid_parenthesis.js
@@ -17,3 +17,25 @@
  */
 
 /** Come up with a solution without thinking about the code */
+
+
+
+const isValidParentheses = function(s){
+    const parens = {
+        "(" : ")",
+        "[" : "]",
+        "{" : "}",
+    }
+    if(s.length === 0) return true;
+    const stack= [];
+    for(let i=0; i<s.length;i++){
+        if(parens[s[i]]){
+            stack.push(s[i]);
+        }else{
+            const leftBracket = stack.pop()
+            const correctBracket = parens[leftBracket]
+            if(s[i] !== correctBracket) return false;
+        }
+    }
+    return stack.length === 0;
+}
